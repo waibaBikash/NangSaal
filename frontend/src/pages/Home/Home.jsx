45b5d3b9ae163da668ac5a53714253d1f78a6cc0
waibaBikash <kavrelibikash@gmail.com>
2024-05-08 19:57:16 +1000

    create a menu item

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import Header from '../../componets/Header/Header';
+import ExploreMenu from '../../componets/ExploreMenu/ExploreMenu';
 
 const Home = () => {
+   const [category, setCategory] = useState("All")
+
   return (
     <div>
     <Header/>
+    <ExploreMenu/>
     </div>
   );
 }
