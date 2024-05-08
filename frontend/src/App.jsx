5aad89c18f8fda828b38405fe558c0eb55ccdf44
waibaBikash <kavrelibikash@gmail.com>
2024-05-08 16:19:49 +1000

    Create a navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './componets/Navbar/Navbar';
 
 function App() {
   return (
-    <div>
-
+    <div className='app'>
+     <Navbar/>
     </div>
   )
 }
 
-export default App
+export default App;
