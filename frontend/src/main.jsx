6064c66b194260426b1c3874c9e7d5748301dcd8
waibaBikash <kavrelibikash@gmail.com>
2024-05-08 21:35:23 +1000

    creare a food list Array

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import { StoreContext } from './context/StoreContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-   <App />
+  <StoreContext.Provider>
+  <App />
+  </StoreContext.Provider>
   </BrowserRouter>
    
 )
