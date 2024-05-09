c9518ce116e3de24c3d0460483c98b263ce98929
waibaBikash <kavrelibikash@gmail.com>
2024-05-09 20:21:46 +1000

    complete a footer section

---
+++ frontend/src/componets/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Footer.css';
+import { assets } from '../../assets/assets';
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+       <div className="footer-content">
+          <div className="footer-content-left">
+             <img src={assets.logo} alt="" />
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quo distinctio est quia quis obcaecati praesentium voluptatem odio maiores porro.</p>
+               <div className="footer-social-icons">
+                 <img src={assets.facebook_icon} alt="" />
+                 <img src={assets.twitter_icon} alt="" />
+                 <img src={assets.linkedin_icon} alt="" />
+               </div>
+          </div>
+          <div className="footer-content-center">
+            <h2>COMPANY</h2>
+             <ul>
+               <li>Home</li>
+               <li>About us</li>
+               <li>Delivery</li>
+               <li>Privacy policy</li>
+             </ul>
+          </div>
+          <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+             <ul>
+               <li>+61-126644169</li>
+               <li>nangsaal@gmail.com.au</li>
+             </ul>
+          </div>
+       </div>
+         <hr />
+         <p className="footer-copyright">
+          Copyright NaangSaal 2024 © naangsaal.com - All Right Reserved.
+         </p>
+    </div>
+  );
+}
+
+export default Footer;
