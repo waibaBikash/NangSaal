19d6053dd381d60adac7f4995ceec635808d21af
waibaBikash <kavrelibikash@gmail.com>
2024-05-10 23:24:23 +1000

    add checkout page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -32,6 +32,37 @@ const Cart = () => {
                )}
            })}
        </div>
+          <div className="cart-bottom">
+               <div className="cart-total">
+                  <h2>Cart Totals</h2>
+                      <div>
+                           <div className="cart-total-details">
+                             <p>Subtotal</p>
+                             <p>{0}</p>
+                           </div>
+                            <hr />
+                           <div className="cart-total-details">
+                             <p>Delivery Fee</p>
+                             <p>{2}</p>
+                           </div>
+                           <hr />
+                           <div className="cart-total-details">
+                              <b>Total</b>
+                               <b>{0}</b>
+                           </div>
+                      </div>
+                       <button>PROCEED TO CHECKOUT</button>
+               </div>
+                 <div className="cart-promocode">
+                    <div>
+                        <p>If you have a promo code, Enter it here</p>
+                         <div className='cart-promocode-input'>
+                            <input type="text" placeholder='promo code' />
+                              <button>Submit</button>
+                         </div>
+                    </div>
+                 </div>
+          </div>
     </div>
   );
 }
