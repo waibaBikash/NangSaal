374863c8e137ded2f580e25ce28f6c548d43ec4c
waibaBikash <kavrelibikash@gmail.com>
2024-05-10 22:01:12 +1000

    add $ sign

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -24,9 +24,9 @@ const Cart = () => {
                   <div className="cart-items-title cart-items-item">
                      <img src={item.image} alt="" />
                      <p>{item.name}</p>
-                     <p>{item.price}</p>
+                     <p>${item.price}</p>
                      <p>{cartItems[item._id]}</p> 
-                    <p>{item.price*cartItems[item._id]}</p> 
+                    <p>${item.price*cartItems[item._id]}</p> 
                       <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
                   </div>
                )}
