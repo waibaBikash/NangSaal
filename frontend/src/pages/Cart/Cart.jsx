96aabb2f7439943dfaa1a94c5f2af68618437bf4
waibaBikash <kavrelibikash@gmail.com>
2024-05-11 23:14:36 +1000

    complte frontend page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -46,12 +46,12 @@ const Cart = () => {
                             <hr />
                            <div className="cart-total-details">
                              <p>Delivery Fee</p>
-                             <p>${getTotalCartAmount()===0?0:2}</p>
+                             <p>${getTotalCartAmount()===0?0:4}</p>
                            </div>
                            <hr />
                            <div className="cart-total-details">
                               <b>Total</b>
-                               <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+                               <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+4}</b>
                            </div>
                       </div>
                        <button onClick={()=>nagigate('/order')}>PROCEED TO CHECKOUT</button>
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -31,19 +31,19 @@ const PlaceOrder = () => {
           <div className="cart-total">
                   <h2>Cart Totals</h2>
                       <div>
-                           <div className="cart-total-details">
+                      <div className="cart-total-details">
                              <p>Subtotal</p>
                              <p>${getTotalCartAmount()}</p>
                            </div>
                             <hr />
                            <div className="cart-total-details">
                              <p>Delivery Fee</p>
-                             <p>${2}</p>
+                             <p>${getTotalCartAmount()===0?0:4}</p>
                            </div>
                            <hr />
                            <div className="cart-total-details">
                               <b>Total</b>
-                               <b>${getTotalCartAmount()+2}</b>
+                               <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+4}</b>
                            </div>
                       </div>
                        <button>PROCEED TO PAYMENT</button>
