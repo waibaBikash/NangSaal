a3c0b6effc35dd5307ab1955e9dfc447da461610
waibaBikash <kavrelibikash@gmail.com>
2024-05-13 23:53:48 +1000

    create a list item page

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './List.css';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const List = () => {
-  return (
-    <div>
 
+  const url = "http://localhost:4000";
+  const [list, setList] = useState([]);
+  
+  const fetchList = async() => {
+    const response = await axios.get(`${url}/api/food/list`);
+    if(response.data.success){
+      setList(response.data.data)
+    }else{
+      toast.error("Error")
+    }
+  }
+
+  useEffect(()=>{
+    fetchList();
+  },[])
+  return (
+    <div className='list add flex-col'>
+        <p>All Food List</p>
+         <div className="list-table">
+            <div className="list-table-format title">
+                <b>Image</b>
+                <b>Name</b>
+                <b>Category</b>
+                <b>Price</b>
+                <b>Action</b>
+            </div>
+             {list.map((item, index)=>{
+               return(
+                  <div key={index} className='list-table-format'>
+                   <img src={`${url}/images/` +item.image} alt="" />
+                   <p>{item.name}</p>
+                   <p>{item.category}</p>
+                   <p>${item.price}</p>
+                   <p className='cursor'>x</p>
+                  </div>
+               )
+             })}
+         </div>
     </div>
   )
 }
