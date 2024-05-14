1f0dd3a25beeef8fc0c30b4b987e0ca70c4ddd2f
waibaBikash <kavrelibikash@gmail.com>
2024-05-14 17:21:29 +1000

    complete login functionality

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,7 +5,7 @@ import { loginUser, registerUser } from '../controllers/userController.js';
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser)
-userRouter.post("login", loginUser)
+userRouter.post("/login", loginUser)
 
 
 export default userRouter;
