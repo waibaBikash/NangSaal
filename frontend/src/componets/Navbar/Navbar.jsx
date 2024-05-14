d1b43339fe695ec24f8e3f5bbdd22566a84805af
waibaBikash <kavrelibikash@gmail.com>
2024-05-14 19:58:15 +1000

    create log out feature

--- frontend/src/componets/Navbar/Navbar.jsx
+++ frontend/src/componets/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({setShowLogin}) => {
    const [menu, setMenu] = useState("home");
-   const {getTotalCartAmount} = useContext(StoreContext);
+   const {getTotalCartAmount, token, setToken} = useContext(StoreContext);
 
   return (
     <div className='navbar'>
@@ -25,7 +25,15 @@ const Navbar = ({setShowLogin}) => {
                 </Link> 
                     <div className={getTotalCartAmount()===0?"":"dot"}></div>
                </div>
-                 <button onClick={()=>setShowLogin(true)}>sign in</button>
+                 {!token?<button onClick={()=>setShowLogin(true)}>sign in</button>:
+                  <div className='navbar-profile'>
+                   <img src={assets.profile_icon} alt="" />
+                    <ul className="nav-profile-dropdown">
+                       <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+                        <hr />
+                        <li><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+                    </ul>
+                  </div>}
            </div>
     </div>
   );
