21947ad9a7d16ae74bfd48920a1eff1de7c1aa03
waibaBikash <kavrelibikash@gmail.com>
2024-05-15 21:11:38 +1000

    remove food list from database

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -39,7 +39,7 @@ const listFood = async (req, res) => {
 const removeFood = async (req, res) => {
   try {
     const food = await foodModel.findById(req.body.id);
-    fs.unlink(`uploads/${food.immage}`, ()=>{})
+    fs.unlink(`uploads/${food.immage}`,()=>{})
     await foodModel.findByIdAndDelete(req.body.id);
     res.json({success:true, message:"Food Removed"});
   } catch (error) {
