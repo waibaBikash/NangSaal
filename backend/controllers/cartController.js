fc9d78353529d76e25028b7f209b2c195a74befd
waibaBikash <kavrelibikash@gmail.com>
2024-05-16 14:24:47 +1000

    remove and fetch data functiionality added

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -23,11 +23,32 @@ const addToCart = async (req,res) => {
 
 // remove items from user cart
 const removeFromCart = async (req,res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+  let cartData = await userData.cartData;
+  if(cartData[req.body.itemId]>0){
+     cartData[req.obdy.itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId,{cartData});
+    res.json({success:true, message:"Remove From Cart"});
+  } catch (error) {
+    console.log(error);
+    res.json({success:false, message:"Error"});
+  }
+  
 
 }
 
 // fetch user cart data
 const getCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let carData = await userData.carData;
+    res.json({success:true, carData});
+  } catch (error) {
+    console.log(error);
+    res.json({success:false, message:"Error"});
+  }
 
 }
 
