08c6dc22df9813184caf521d5ea631b86c70b186
waibaBikash <kavrelibikash@gmail.com>
2024-05-18 20:44:39 +1000

    create a order updata functionality

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -90,5 +90,14 @@ const listOrders = async (req,res) => {
   }
 
 }
-
-export {placeOrder,verifyOrder,userOrder,listOrders}
+// api for updating order status
+ const updataStatus = async (req, res) => {
+    try {
+      await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+      res.json({success:true, message:"Status Updated"});
+    } catch (error) {
+      console.log(error);
+      res.json({success:false, message:"Error"});
+    }
+ }
+export {placeOrder,verifyOrder,userOrder,listOrders,updataStatus}
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder, userOrder, verifyOrder } from '../controllers/orderController.js';
+import { listOrders, placeOrder, updataStatus, userOrder, verifyOrder } from '../controllers/orderController.js';
 
 
 const orderRouter = express.Router();
@@ -8,7 +8,8 @@ const orderRouter = express.Router();
 orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userordes", authMiddleware, userOrder);
-orderRouter.get("/list", listOrder)
+orderRouter.get("/list", listOrders);
+orderRouter.post("/status", updataStatus);
 
 
 export default orderRouter;
