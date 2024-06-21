4d644ebafc09f05e80ce7ddd085a91dfc35d87d9
waibaBikash <kavrelibikash@gmail.com>
2024-06-21 17:03:27 +1000

    initial change

--- frontend/src/componets/AppDownload/AppDownload.jsx
+++ frontend/src/componets/AppDownload/AppDownload.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../assets/assets';
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
-      <p>For Better Experience Download <br />NangSaal App</p>
+      <p>For Better Experience Download <br />NangSal App</p>
        <div className="app-download-platforms">
          <img src={assets.app_store} alt="" />
          <img src={assets.play_store} alt="" />
