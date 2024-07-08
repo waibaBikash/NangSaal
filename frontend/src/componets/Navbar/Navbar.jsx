e0dd8a96048c09febcf4393ede07ff3358b462a9
waibaBikash <kavrelibikash@gmail.com>
2024-07-08 10:10:30 +1000

    commit change

--- frontend/src/componets/Navbar/Navbar.jsx
+++ frontend/src/componets/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets';
 import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 
-const Navbar = ({setShowLogin}) => {
+   const Navbar = ({setShowLogin}) => {
    const [menu, setMenu] = useState("home");
    const {getTotalCartAmount, token, setToken} = useContext(StoreContext);
 
