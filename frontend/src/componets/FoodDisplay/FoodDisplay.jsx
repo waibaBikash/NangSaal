af59316543c99935dadd76756b60e3b04a337c0f
waibaBikash <kavrelibikash@gmail.com>
2024-07-08 10:02:07 +1000

    initial change

--- frontend/src/componets/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/componets/FoodDisplay/FoodDisplay.jsx
@@ -4,8 +4,7 @@ import { StoreContext } from '../../context/StoreContext';
 import FoodItem from '../FoodItem/FoodItem';
 
 const FoodDisplay = ({category}) => {
-
-   const {food_list} = useContext(StoreContext);
+  const {food_list} = useContext(StoreContext);
   return (
     <div className='food-display' id='food-display'>
        <h2>Top dishes near you</h2>
