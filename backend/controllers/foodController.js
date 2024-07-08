5662b7599c6bfe42aa321d1dc0c2d0fd0d619a7f
waibaBikash <kavrelibikash@gmail.com>
2024-07-08 10:21:16 +1000

    save change

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -6,7 +6,7 @@ import fs from 'fs';
 
 const addFood = async(req, res) => {
 
-  let image_filename = `${req.file.filename}`;
+let image_filename = `${req.file.filename}`;
 
   const food = new foodModel({
     name:req.body.name,
