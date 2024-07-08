f899dc84114f87bbb4ef457cdcb578085fe8c730
waibaBikash <kavrelibikash@gmail.com>
2024-07-08 10:03:01 +1000

    commit change

--- frontend/src/componets/FoodItem/FoodItem.jsx
+++ frontend/src/componets/FoodItem/FoodItem.jsx
@@ -5,7 +5,7 @@ import { StoreContext } from '../../context/StoreContext';
 
 const FoodItem = ({id,name,price,description,image}) => {
 
-   const {cartItems, addToCart, removeFromCart, url} = useContext(StoreContext);
+  const {cartItems, addToCart, removeFromCart, url} = useContext(StoreContext);
   return (
     <div className='food-item'>
        <div className="food-item-img-container">
