33a69f770977cc3021b13f7a2adf8b9576a63bf5
waibaBikash <kavrelibikash@gmail.com>
2024-07-08 10:13:18 +1000

    save change

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -45,7 +45,6 @@ const StoreContextProvider = (props) => {
        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}});
        setCartItems(response.data.cartData);
      }
-
     useEffect(()=>{
         async function loadData(){
           await fetchFoodList();
