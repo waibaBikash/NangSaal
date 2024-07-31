0f7d13edab847efe789121fe9b70f89413ccb17f
waibaBikash <kavrelibikash@gmail.com>
2024-07-31 22:45:45 +1000

    commit change

--- frontend/src/componets/Header/Header.jsx
+++ frontend/src/componets/Header/Header.jsx
@@ -6,7 +6,8 @@ const Header = () => {
     <div className='header'>
        <div className="header-contents">
           <h2>Order your favourite food here</h2>
-          <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and elevate your dining experience, one dilicious meal at a time. </p>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and elevate your dining experience, one dilicious meal at a time.
+            </p>
           <button>View Menu</button>
        </div>
     </div>
