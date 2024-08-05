03a41319f755f829614c9a0a367cfcf41ad74053
waibaBikash <kavrelibikash@gmail.com>
2024-08-05 22:14:10 +1000

    save change

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,6 +1,7 @@
 import userModel from '../models/userModel.js';
 
 // add items to user cart
+
 const addToCart = async (req,res) => {
   try {
     let userData = await userModel.findById(req.body.userId);
