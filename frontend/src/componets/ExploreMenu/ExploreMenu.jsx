928feb786b0a86e62abd3f13c53c2f23cd835aaa
waibaBikash <kavrelibikash@gmail.com>
2024-08-06 22:31:16 +1000

    save change

--- frontend/src/componets/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/componets/ExploreMenu/ExploreMenu.jsx
@@ -5,7 +5,7 @@ import { menu_list } from '../../assets/assets';
 const ExploreMenu = ({category, setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
-    <h1>Explore our menu</h1>
+    <h1>Explore Our Menu</h1>
     <p className='explore-menu-text'>
       Choose from a diverse menu featuring a delectable array of deshes. Our mission is to satisfy your craving and elevate your dining experience, one delicious meal at a time.
     </p>
