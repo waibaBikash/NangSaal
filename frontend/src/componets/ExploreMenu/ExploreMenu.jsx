3b4e562ca61b71a217793c0af4b80be0ae104767
waibaBikash <kavrelibikash@gmail.com>
2024-08-06 22:31:41 +1000

    commit change

--- frontend/src/componets/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/componets/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './ExploreMenu.css';
 import { menu_list } from '../../assets/assets';
 
-const ExploreMenu = ({category, setCategory}) => {
+const ExploreMenu = ({category,
+   setCategory
+  }) => {
   return (
     <div className='explore-menu' id='explore-menu'>
     <h1>Explore Our Menu</h1>
