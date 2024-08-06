8d8c00cbb473219cdd20729b187cc41ae1f27327
waibaBikash <kavrelibikash@gmail.com>
2024-08-06 22:34:10 +1000

    commit change

--- frontend/src/componets/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/componets/FoodDisplay/FoodDisplay.jsx
@@ -11,7 +11,8 @@ const FoodDisplay = ({category}) => {
         <div className="food-display-list">
           {food_list.map((item, index)=>{
             if(category==='All' || category===item
-              .category){
+              .category)
+              {
             return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
             }
           })}
