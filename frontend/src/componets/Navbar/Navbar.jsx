031498f2cda43e6ee916f68af702ed6db5a63e38
waibaBikash <kavrelibikash@gmail.com>
2024-08-10 20:42:40 +1000

    save change

--- frontend/src/componets/Navbar/Navbar.jsx
+++ frontend/src/componets/Navbar/Navbar.jsx
@@ -27,7 +27,8 @@ import { StoreContext } from '../../context/StoreContext';
            className={menu==="menu"?"active":""}>menu</a>
            <a href='#app-download' onClick={()=>setMenu("mobile-app")} 
            className={menu==="mobile-app"?"active":""}>mobile-app</a>
-           <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
+           <a href='#footer' onClick={()=>setMenu("contact-us")} 
+           className={menu==="contact-us"?"active":""}>contact us</a>
         </ul>
            <div className="navbar-right">
               <img src={assets.search_icon} alt="" />
