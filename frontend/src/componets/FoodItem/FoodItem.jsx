0a4aa5dcc11f82293d373151be9e1db3169f89e3
waibaBikash <kavrelibikash@gmail.com>
2024-08-10 20:37:04 +1000

    save change

--- frontend/src/componets/FoodItem/FoodItem.jsx
+++ frontend/src/componets/FoodItem/FoodItem.jsx
@@ -22,7 +22,8 @@ const FoodItem = ({id,
             : <div className='food-item-counter'>
                <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
                <p>{cartItems[id]}</p>
-                <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
+                <img onClick={()=>addToCart(id)} 
+                src={assets.add_icon_green} alt="" />
             </div>
           }
        </div>
