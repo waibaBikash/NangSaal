5a44a24616a694cf67b6f1af282fd0b17197f79a
waibaBikash <kavrelibikash@gmail.com>
2024-08-10 20:46:22 +1000

    save change

--- frontend/src/componets/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/componets/ExploreMenu/ExploreMenu.jsx
@@ -9,7 +9,9 @@ const ExploreMenu = ({category,
     <div className='explore-menu' id='explore-menu'>
     <h1>Explore Our Menu</h1>
     <p className='explore-menu-text'>
-      Choose from a diverse menu featuring a delectable array of deshes. Our mission is to satisfy your craving and elevate your dining experience, one delicious meal at a time.
+      Choose from a diverse menu featuring a delectable array of deshes. 
+      Our mission is to satisfy your craving and elevate your dining 
+      experience, one delicious meal at a time.
     </p>
       <div className="explore-menu-list">
          {menu_list.map((item, index)=>{
@@ -17,7 +19,8 @@ const ExploreMenu = ({category,
              <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)}
              key={index}
              className="explore-menu-list-item">
-                <img  className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+                <img  className={category===item.menu_name?"active":""}
+                 src={item.menu_image} alt="" />
                 <p>
                   {item.menu_name}
                 </p>
