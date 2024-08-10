c6081b9e9fbe969f9611bfb869b2bab067f7cfda
waibaBikash <kavrelibikash@gmail.com>
2024-08-10 20:51:18 +1000

    commit change

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -80,14 +80,19 @@ const PlaceOrder = () => {
               <input required name='street' onChange={onChangeHandler} 
               value={data.street} type="text" placeholder='Street' />
                <div className="multi-fields">
-                 <input required name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City' />
-                 <input required name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='State' />
+                 <input required name='city' onChange={onChangeHandler} 
+                 value={data.city} type="text" placeholder='City' />
+                 <input required name='state' onChange={onChangeHandler} 
+                 value={data.state} type="text" placeholder='State' />
                </div>
                <div className="multi-fields">
-                 <input required name='zipcode' onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='Zip code' />
-                 <input required name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country' />
+                 <input required name='zipcode' onChange={onChangeHandler} 
+                 value={data.zipcode} type="text" placeholder='Zip code' />
+                 <input required name='country' onChange={onChangeHandler} 
+                 value={data.country} type="text" placeholder='Country' />
                </div>
-                <input required name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder='Phone' />
+                <input required name='phone' onChange={onChangeHandler}
+                 value={data.phone} type="text" placeholder='Phone' />
         </div>
           <div className="place-order-right">
           <div className="cart-total">
