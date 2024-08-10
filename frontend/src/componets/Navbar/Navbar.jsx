050c91e64b9d4c2d7457841fe611f9905e24b796
waibaBikash <kavrelibikash@gmail.com>
2024-08-10 20:41:59 +1000

    commit change

--- frontend/src/componets/Navbar/Navbar.jsx
+++ frontend/src/componets/Navbar/Navbar.jsx
@@ -21,7 +21,8 @@ import { StoreContext } from '../../context/StoreContext';
     <div className='navbar'>
       <Link to='/'><img src={assets.logo} alt="" className='logo' /> </Link>
         <ul className="navbar-menu">
-           <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+           <Link to='/' onClick={()=>setMenu("home")} 
+           className={menu==="home"?"active":""}>home</Link>
            <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
            <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
            <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
