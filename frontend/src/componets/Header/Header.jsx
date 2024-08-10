6de46ab5a567d9f4555a5c444ea79145af8c31df
waibaBikash <kavrelibikash@gmail.com>
2024-08-10 20:38:20 +1000

    save change

--- frontend/src/componets/Header/Header.jsx
+++ frontend/src/componets/Header/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <div className='header'>
        <div className="header-contents">
-          <h2>Order your favourite food here</h2>
+          <h2>
+            Order your favourite food here
+          </h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and elevate your dining experience, one dilicious meal at a time.
             </p>
           <button>View Menu</button>
