66c6c73b5edf082a6ecbce5b019bf2d588523407
waibaBikash <kavrelibikash@gmail.com>
2024-08-10 20:47:23 +1000

    commit change

--- frontend/src/componets/FoodItem/FoodItem.jsx
+++ frontend/src/componets/FoodItem/FoodItem.jsx
@@ -18,9 +18,11 @@ const FoodItem = ({id,
          <img className='food-item-image' 
          src={url+"/images/"+image} alt="" />
           {!cartItems[id]
-            ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt=''/>
+            ?<img className='add' onClick={()=>addToCart(id)} 
+            src={assets.add_icon_white} alt=''/>
             : <div className='food-item-counter'>
-               <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+               <img onClick={()=>removeFromCart(id)}
+                src={assets.remove_icon_red} alt="" />
                <p>{cartItems[id]}</p>
                 <img onClick={()=>addToCart(id)} 
                 src={assets.add_icon_green} alt="" />
