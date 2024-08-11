f87053ebe8108adba20a1c7078aabdf5bffaf6e4
waibaBikash <kavrelibikash@gmail.com>
2024-08-11 23:52:19 +1000

    commit change

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -10,7 +10,8 @@ const MyOrders = () => {
   const [data,setData] = useState([]);
 
   const fetchOrders = async () => {
-     const response = await axios.post(url+"/api/order/userorders", {},{headers:{token}});
+     const response = await axios
+     .post(url+"/api/order/userorders", {},{headers:{token}});
      setData(response.data.data);
   }
    useEffect(()=>{
