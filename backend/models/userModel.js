36dad2b1760d0f265040a8943d481577173f4c9a
waibaBikash <kavrelibikash@gmail.com>
2024-08-12 23:28:11 +1000

    save change

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema({
   email:{type:String,
     required:true,
     unique:true},
-  password:{type:String,required:true},
+  password:{type:String,
+    required:true},
   cartData:{type:Object,default:{}}
 },{minimize:false});
 
