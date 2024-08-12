50639d87a27c4c4fee3014f2427d3f99dfd44bd8
waibaBikash <kavrelibikash@gmail.com>
2024-08-12 23:25:13 +1000

    commit change

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,8 @@ import jwt from 'jsonwebtoken';
 const authMiddleware = async (req,res,next) => {
    const {token} = req.headers;
    if(!token){
-      return res.json({success:false, message:"Not Authorized Login Again"})
+      return res.json({success:false,
+         message:"Not Authorized Login Again"})
    }
      try {
       const token_decode = jwt.verify(token, process.env.JWT_SECRET);
