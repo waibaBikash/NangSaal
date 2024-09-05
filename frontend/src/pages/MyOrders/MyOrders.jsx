cd312a4771fca2cb0c50fd906a97a232284a9fa2
waibaBikash <kavrelibikash@gmail.com>
2024-09-05 22:09:02 +1000

    save change

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -37,7 +37,9 @@ const MyOrders = () => {
                      <p>${order.amount}.00</p>
                      <p>Items:{order.items.length}</p>
                      <p><span>&#x25cf;</span><b>{order.status}</b></p>
-                     <button onClick={fetchOrders}>Track Order</button>
+                     <button 
+                      onClick={fetchOrders}>Track Order
+                      </button>
                  </div>
               )
             })}
