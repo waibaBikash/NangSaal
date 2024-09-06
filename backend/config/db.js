4f2f82bcfc887845a55ec921a227ac0bbc7545ac
waibaBikash <kavrelibikash@gmail.com>
2024-09-06 22:51:22 +1000

    save change

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,6 @@ import mongoose from "mongoose";
 
 
  export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://kavrelibikash:mayalu2425@cluster0.bqlw3tx.mongodb.net/NangSaal').then(()=>console.log("DB Connected"));
+  await mongoose.connect('mongodb+srv://kavrelibikash:mayalu2425@cluster0.bqlw3tx.mongodb.net/NangSaal')
+  .then(()=>console.log("DB Connected"));
 }
