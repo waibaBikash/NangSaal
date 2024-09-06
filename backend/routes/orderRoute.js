452729a217c0d967d4d7ddcd27116091f3ab4b86
waibaBikash <kavrelibikash@gmail.com>
2024-09-06 22:57:35 +1000

    commit change

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.js';
-import { listOrders, placeOrder, updateStatus, userOrder, verifyOrder } from '../controllers/orderController.js';
+import { listOrders,
+   placeOrder,
+    updateStatus, 
+    userOrder, 
+    verifyOrder } from '../controllers/orderController.js';
 
 
 const orderRouter = express.Router();
