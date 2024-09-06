c2c587f07c46a4d92ca7750eb70b3e33c462fb06
waibaBikash <kavrelibikash@gmail.com>
2024-09-06 22:56:41 +1000

    commit change

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import { addToCart,removeFromCart, getCart } from '../controllers/cartController.js';
+import { addToCart,
+  removeFromCart, 
+  getCart } from '../controllers/cartController.js';
 import authMiddleware from '../middleware/auth.js';
 
 const cartRouter = express.Router();
