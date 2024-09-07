46c19843d50d3fb51064afe15e37866cb905a00d
waibaBikash <kavrelibikash@gmail.com>
2024-09-07 21:47:48 +1000

    changes made

--- frontend/src/componets/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/componets/FoodDisplay/FoodDisplay.jsx
@@ -16,8 +16,11 @@ const FoodDisplay = ({category}) => {
             if(category==='All' || category===item
               .category)
               {
-            return <FoodItem key={index} id={item._id} name={item.name} 
-            description={item.description} price={item.price} image={item.image}/>
+            return <FoodItem key={index} id={item._id} 
+            name={item.name} 
+            description={item.description}
+             price={item.price} image={item.image}
+             />
             }
           })}
         </div>
