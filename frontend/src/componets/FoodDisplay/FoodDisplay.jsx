2493efe928b048e0ff98dc5e6bffe9f5c16b6e9a
waibaBikash <kavrelibikash@gmail.com>
2024-09-07 21:47:00 +1000

    commit change

--- frontend/src/componets/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/componets/FoodDisplay/FoodDisplay.jsx
@@ -6,8 +6,11 @@ import FoodItem from '../FoodItem/FoodItem';
 const FoodDisplay = ({category}) => {
   const {food_list} = useContext(StoreContext);
   return (
-    <div className='food-display' id='food-display'>
-       <h2>Top dishes near you</h2>
+    <div 
+      className='food-display' id='food-display'>
+       <h2>
+        Top dishes near you
+        </h2>
         <div className="food-display-list">
           {food_list.map((item, index)=>{
             if(category==='All' || category===item
