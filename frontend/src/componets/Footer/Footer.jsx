8cb96acab593e615afabb9695ddd9a1b102756e7
waibaBikash <kavrelibikash@gmail.com>
2024-09-09 21:51:45 +1000

    save change

--- frontend/src/componets/Footer/Footer.jsx
+++ frontend/src/componets/Footer/Footer.jsx
@@ -29,7 +29,9 @@ const Footer = () => {
           <div className="footer-content-center">
             <h2>COMPANY</h2>
              <ul>
-               <li>Home</li>
+                <li>
+                Home
+                </li>
                <li>About us</li>
                <li>Delivery</li>
                <li>Privacy policy</li>
