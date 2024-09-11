ebe855b4516b2b09a2e943f10af49012b41c621b
waibaBikash <kavrelibikash@gmail.com>
2024-09-11 20:27:13 +1000

    save chnage

--- frontend/src/componets/Header/Header.jsx
+++ frontend/src/componets/Header/Header.jsx
@@ -4,7 +4,8 @@ import './Header.css';
 const Header = () => {
   
   return (
-    <div className='header'>
+    <div 
+      className='header'>
        <div 
         className="header-contents">
           <h2>
